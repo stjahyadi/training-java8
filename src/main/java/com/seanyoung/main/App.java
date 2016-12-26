186ce5922f950623fc017e4200de7b6eafaee646
stjahyadi <seanyoung.tjahyadi@gmail.com>
2016-12-26 23:11:57 +0800

    Initial Commit

---
+++ src/main/java/com/seanyoung/main/App.java
@@ -0,0 +1,20 @@
+package com.seanyoung.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+  public static void forEachJava8() {
+    List<String> data = new ArrayList<String>();
+    data.add("training");
+    data.add("java");
+    data.add("8");
+
+    //print using method reference
+    data.forEach(System.out::println);
+  }
+
+  public static void main(String[] args) {
+    forEachJava8();
+  }
+}
