8268c448aa5b01e8fb6e4b1ce556e1da8cdb43c2
Sean Young <seanyoung.tjahyadi@izeno.com>
2018-08-16 13:37:52 +0800

    Add employee object

---
+++ src/main/java/com/seanyoung/main/Employee.java
@@ -0,0 +1,15 @@
+
+class Employee{
+	private String name;
+	
+	public Employee(){
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
